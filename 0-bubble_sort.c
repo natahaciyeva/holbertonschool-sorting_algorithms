506a1240052ada2e02be14acb8ad592800b02e92
natahaciyeva <natiagadjieva@gmail.com>
2025-10-05 08:27:16 +0000

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the Bubble sort algorithm
+ * @array: The array to be sorted
+ * @size: Number of elements in the array
+ *
+ * Description: Prints the array after every swap.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, n;
+	int temp, changed;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (n = size; n > 1; n--)
+	{
+		changed = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+				changed = 1;
+			}
+		}
+		if (!changed)
+			break;
+	}
+}
